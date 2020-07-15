a974b5ea3a77ca0134cb61ac21d415ae9cce2918
h_kojima <h_kojima@smiloops.com>
2020-07-16 01:28:56 +0900

    Add sort algorithms.

--- sort/sort.go
+++ sort/sort.go
@@ -3,6 +3,7 @@ package sort
 type Sort interface {
 	SelectionSort() ([]int, error)
 	BubbleSort() ([]int, error)
+	InsertionSort() ([]int, error)
 	QuickSort() ([]int, error)
 }
 
@@ -17,13 +18,97 @@ func NewSort(list []int) Sort {
 }
 
 func (s *sort) SelectionSort() ([]int, error) {
+	for i := 0; i < len(s.List); i++ {
+		m := i
+		for k := i + 1; k < len(s.List); k++ {
+			if s.List[i] > s.List[k] {
+				m = k
+			}
+		}
+
+		if i != m {
+			w := s.List[i]
+			s.List[i] = s.List[m]
+			s.List[m] = w
+		}
+	}
 	return s.List, nil
 }
 
 func (s *sort) BubbleSort() ([]int, error) {
+	for i := 0; i < len(s.List); i++ {
+		for k := len(s.List) - 1; k > i; k-- {
+			if s.List[k] < s.List[k - 1] {
+				w := s.List[k]
+				s.List[k] = s.List[k - 1]
+				s.List[k-1] = w
+			} 
+		}
+	}
+	return s.List, nil
+}
+
+func (s *sort) InsertionSort() ([]int, error) {
+	for i := 1; i < len(s.List); i++ {
+		w := s.List[i]
+		for k := i; k >= 0; k-- {
+			if k == 0 || s.List[k - 1] < w {
+				s.List[k] = w
+				break
+			} else {
+				s.List[k] = s.List[k - 1]
+			}
+		}
+	}
 	return s.List, nil
 }
 
 func (s *sort) QuickSort() ([]int, error) {
+	left := 0
+	right := len(s.List) - 1
+
+	s.subQuickSort(left, right)
+
 	return s.List, nil
+}
+
+func (s *sort) subQuickSort(left int, right int) error {	
+	i := left + 1
+	k := right
+
+	for i < k {
+		for i < right {
+			if s.List[i] > s.List[left] {
+				break
+			}
+			i++
+		}
+		for k > left {
+			if s.List[k] <= s.List[left] {
+				break
+			}
+			k--
+		}
+
+		if i < k {
+			w := s.List[i]
+			s.List[i] = s.List[k]
+			s.List[k] = w
+		}
+	}
+
+	if s.List[left] > s.List[k] {
+		w := s.List[left]
+		s.List[left] = s.List[k]
+		s.List[k] = w
+	}
+
+	if left + 1 < k {
+		s.subQuickSort(left, k - 1)
+	}
+	if right -1 > k {
+		s.subQuickSort(k + 1, right)
+	}
+
+	return nil
 }
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,1 +1,119 @@
 package sort
+
+import (
+	"math/rand"
+	"reflect"
+	"testing"
+)
+
+func TestSelectionSort(t *testing.T) {
+	expect := []int{0,1,2,3,4,5,6,7,8,9}
+	list := []int{0,9,8,7,6,5,4,3,2,1}
+
+	sort := NewSort(list)
+
+	sort.SelectionSort()
+
+	if !reflect.DeepEqual(list, expect) {
+		t.Error(list)
+	}
+}
+
+func TestBubbleSort(t *testing.T) {
+	expect := []int{0,1,2,3,4,5,6,7,8,9}
+	list := []int{0,9,8,7,6,5,4,3,2,1}
+
+	sort := NewSort(list)
+
+	sort.BubbleSort()
+
+	if !reflect.DeepEqual(list, expect) {
+		t.Error(list)
+	}
+}
+
+func TestInsertionSort(t *testing.T) {
+	expect := []int{0,1,2,3,4,5,6,7,8,9}
+	list := []int{0,9,8,7,6,5,4,3,2,1}
+
+	sort := NewSort(list)
+
+	sort.InsertionSort()
+
+	if !reflect.DeepEqual(list, expect) {
+		t.Error(list)
+	}
+}
+
+func TestQuickSort(t *testing.T) {
+	expect := []int{0,1,2,3,4,5,6,7,8,9}
+	list := []int{0,9,8,7,6,5,4,3,2,1}
+
+	sort := NewSort(list)
+
+	sort.QuickSort()
+
+	if !reflect.DeepEqual(list, expect) {
+		t.Error(list)
+	}
+}
+
+func BenchmarkSelectionSort(b *testing.B) {
+	list := []int{}
+	for i := 0; i < 10000; i++ {
+		list = append(list, rand.Intn(10000))
+	}
+
+	sort := NewSort(list)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		sort.SelectionSort()
+	}
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	list := []int{}
+	for i := 0; i < 10000; i++ {
+		list = append(list, rand.Intn(10000))
+	}
+
+	sort := NewSort(list)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		sort.BubbleSort()
+	}
+}
+
+func BenchmarkInsertionSort(b *testing.B) {
+	list := []int{}
+	for i := 0; i < 10000; i++ {
+		list = append(list, rand.Intn(10000))
+	}
+
+	sort := NewSort(list)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		sort.InsertionSort()
+	}
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	list := []int{}
+	for i := 0; i < 10000; i++ {
+		list = append(list, rand.Intn(10000))
+	}
+
+	sort := NewSort(list)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		sort.QuickSort()
+	}
+}
